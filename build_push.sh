a361db83075a8041d16aa91c3031aa897070a8b9
Nordes M.-Lamarre <nordes.menardlamarre@bell.ca>
2022-05-11 15:16:41 -0400

    build cpu + go

--- build_push.sh
+++ build_push.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-VERSION="0.2"
+VERSION="0.3"
 
 # for var in cpu cpu-noopt gpu gpu-cc53 gpu-cc60 gpu-cc61 gpu-cc62 gpu-cc70 gpu-cc72 gpu-cc75 gpu-cc80 gpu-cc86 \
 #           cpu-cv cpu-noopt-cv gpu-cv gpu-cv-cc53 gpu-cv-cc60 gpu-cv-cc61 gpu-cv-cc62 gpu-cv-cc70 gpu-cv-cc72 gpu-cv-cc75 gpu-cv-cc80 gpu-cv-cc86
-for var in gpu-cc75
+for var in gpu-cc75 cpu-go
 do
   DOCKER_REPO="nordesbellnet/darknet"
   SOURCE_BRANCH="master"
@@ -25,14 +25,14 @@ if [[ "$DOCKER_TAG" == *cv* ]]; then
       --build-arg CONFIG=$VAR \
       --build-arg SOURCE_BRANCH=$SOURCE_BRANCH \
       --build-arg SOURCE_COMMIT=$SOURCE_COMMIT \
-      -t $DOCKER_REPO:$DOCKER_TAG-$VERSION -f Dockerfile.cpu-cv .
+      -t $DOCKER_REPO:$VERSION-$DOCKER_TAG -f Dockerfile.cpu-cv .
   else
     echo "building gpu-cv"
     docker build \
       --build-arg CONFIG=$VAR \
       --build-arg SOURCE_BRANCH=$SOURCE_BRANCH \
       --build-arg SOURCE_COMMIT=$SOURCE_COMMIT \
-      -t $DOCKER_REPO:$DOCKER_TAG-$VERSION -f Dockerfile.gpu-cv .
+      -t $DOCKER_REPO:$VERSION-$DOCKER_TAG -f Dockerfile.gpu-cv .
   fi
  else
   if [[ "$DOCKER_TAG" = *cpu || "$DOCKER_TAG" = *cpu-noopt ]]; then
@@ -41,14 +41,21 @@ if [[ "$DOCKER_TAG" == *cv* ]]; then
       --build-arg CONFIG=$VAR \
       --build-arg SOURCE_BRANCH=$SOURCE_BRANCH \
       --build-arg SOURCE_COMMIT=$SOURCE_COMMIT \
-      -t $DOCKER_REPO:$DOCKER_TAG-$VERSION -f Dockerfile.cpu .
+      -t $DOCKER_REPO:$VERSION-$DOCKER_TAG -f Dockerfile.cpu .
+  elif [[ "$DOCKER_TAG" = *cpu-go || "$DOCKER_TAG" = *cpu-go-noopt ]]; then
+    echo "building cpu-go or cpu-noopt-go"
+    docker build \
+      --build-arg CONFIG=$VAR \
+      --build-arg SOURCE_BRANCH=$SOURCE_BRANCH \
+      --build-arg SOURCE_COMMIT=$SOURCE_COMMIT \
+      -t $DOCKER_REPO:$VERSION-$DOCKER_TAG -f Dockerfile.cpu-go .
   else
     echo "building gpu"
     docker build \
       --build-arg CONFIG=$VAR \
       --build-arg SOURCE_BRANCH=$SOURCE_BRANCH \
       --build-arg SOURCE_COMMIT=$SOURCE_COMMIT \
-      -t $DOCKER_REPO:$DOCKER_TAG-$VERSION -f Dockerfile.gpu .
+      -t $DOCKER_REPO:$VERSION-$DOCKER_TAG -f Dockerfile.gpu .
   fi
 fi
 
